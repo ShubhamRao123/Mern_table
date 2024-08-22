be563751527932afc63bd700508484cf4cf0fed2
Shubham Rao <shubhamrao834@gmail.com>
2024-08-22 07:31:21 +0530

    completed client side

---
+++ src/UpdateUser.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function UpdateUser() {
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <form>
+          <h2>Update User</h2>
+          <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              placeholder="Enter your Name"
+              className="form-control"
+            ></input>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              className="form-control"
+            ></input>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Age</label>
+            <input
+              type="text"
+              placeholder="Enter your Age"
+              className="form-control"
+            ></input>
+          </div>
+          <button className="btn btn-success">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default UpdateUser;
